195062bfc1c4cb19662f3f6d9acdf3964d2a868a
Pyarel <pyarel.fer@gmail.com>
2023-10-23 22:00:38 -0400

    React - Day 1, Commit 2

---
+++ src/BlogList.js
@@ -0,0 +1,19 @@
+const BlogList = ({ blogs, title, handleDelete }) => {
+  //   const blogs = props.blogs;
+  //   const title = props.title;
+
+  return (
+    <div className="blog-list">
+      <h1>{title}</h1>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written By: {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;
---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+const Home = () => {
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
+  return (
+    <div className="Home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading....</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+    </div>
+  );
+};
+
+export default Home;
